41f738f4858aad2bd93d21e701e09e7a0d7255cb
unknown <kumarrohitsingh824@gmail.com>
2022-10-21 02:20:33 +0530

    please correct palindrome and prime number code

---
+++ prime.js
@@ -0,0 +1,22 @@
+ let str= "naman";
+    status=true;
+for(i=0;i<=str.length;i++){
+    if(str[i]!==str[str.length-i]){
+      status=false;  
+    }
+    
+}
+if(status==false){console.log("No");}
+else{console.log("Yes");}
+}
+
+
+let num=13;
+let count=0;
+for(let i=1;i<=num;i++){
+    if(num/i==0){
+    count--;}}
+if(count=2){console.log("Yes");
+}else{console.log("No")}
+}
+
